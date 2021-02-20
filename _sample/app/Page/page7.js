acb22747fd5acf2539002ef748190588a2e19d06
nakatsuji <nktj_takakazu2014@yahoo.co.jp>
2021-02-20 22:25:42 +0900

    2/20

--- _sample/app/Page/page7.js
+++ _sample/app/Page/page7.js
@@ -11,39 +11,31 @@ rd2.page("page7").before(function(obj){
                 aaa:"bbbbbb",
                 ccc:"dddd",
             },
-            callbacks:{
-                before:function(cond){
-
-                    console.log("ajax before...");
-                    cond.headers.user="usernames";
-
-                },
-                beforeSend:function(){
-
-                    console.log("ajax before send...");
-                },
-            },
         }).send();
         
         req.done(function(data){
             alert(data);
-            console.log(data);
         }).error(function(err){
-            alert("ERROR!!");
-            throw(err);
+            alert(err);
         });
 
     });
 
     obj.pageObj.find(".request2").on("click",function(){
 
-        var req=rd2.request("type_b").url("test01.php").send();
-        
+        var req=rd2.request("type_b").set({
+            url:"test01.php",
+            method:"post",
+            data:{
+                aaa:"bbbbbb",
+                ccc:"dddd",
+            },
+        }).send();
+
         req.done(function(data){
             alert(data);
         }).error(function(err){
-            alert("ERROR!!");
-            throw(err);
+            alert(err);
         });
 
     });
