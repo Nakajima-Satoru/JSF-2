5983289a4b21db17f195ab1a7544032764858836
nakatsuji masato <nakatsuji@teastalk.jp>
2021-02-21 12:54:29 +0900

    2/21

---
+++ template/simple/app/Group/app.js
@@ -0,0 +1,19 @@
+
+/***
+ * --------------------------------------
+ * JSF-2 Group Script
+ * app
+ * --------------------------------------
+ */
+rd2.group("app").set({
+
+	/**
+	 * before
+	 * @param obj
+	 */
+	before:function(obj){
+
+		// before callback area...
+	},
+	
+});
---
+++ template/simple/app/Page/top.js
@@ -0,0 +1,44 @@
+/***
+ * --------------------------------------
+ * JSF-2 Page Script
+ * top
+ * --------------------------------------
+ */
+rd2.page("top").set({
+	
+	/**
+	 * addGroup
+	 */
+	addGroup:["app"],
+
+	/**
+	 * before
+	 * @param obj
+	 */
+	before:function(obj){
+
+		// before callback area...
+
+	},
+
+	/**
+	 * after
+	 * @param obj
+	 */
+	after:function(obj){
+
+		// after callback area...
+
+	},
+
+	/**
+	 * leave
+	 * @param obj
+	 */
+	leave:function(obj){
+
+		// learv callback area...
+
+	},
+	
+});
